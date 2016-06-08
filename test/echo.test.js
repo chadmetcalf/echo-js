7dd911d36d6294f3541a8c78002f0bdea5dc1ffe
Chad Metcalf <cmetcalf@rednovalabs.com>
2016-06-08 13:47:16 -0500

    init commit

---
+++ test/echo.test.js
@@ -0,0 +1,23 @@
+var echo = require('../src/echo.js');
+var expect = require('expect.js');
+
+describe('echo client', function() {
+  describe('#init', function() {
+    xit('requires a user id')
+    xit('aims at notification app')
+  });
+
+  describe('#notifications', function() {
+    describe('sends request', function() {
+      xit('formats data')
+      xit('has callback')
+    });
+  });
+
+  describe('callback?', function() {
+    describe('processes response', function() {
+      xit('understand payload format')
+      xit('prepares data for consumption')
+    });
+  });
+});
