dd639d88452ad2b17c58780405fa09c44e030a9b
kxhitiz <nirvanagrg@gmail.com>
2016-06-09 16:40:19 -0500

    Add acknowledge notification interface.

--- src/echo.js
+++ src/echo.js
@@ -1,20 +1,31 @@
-  var echo = {
-    options: {
-      appId: 'dummyAppID',
-      userId: 'dummyUserID',
-      baseUrl: 'http://localhost:4000/api/v1'
-    },
+var echo = {
+  options: {
+    appId: 'dummyAppID',
+    userId: 'dummyUserID',
+    baseUrl: 'http://localhost:4000/api/v1'
+  },
 
-    init: function(appId, userId, options) {
-      this.appId = appId;
-      this.userId = userId;
-      $.extend(this.options, options);
-    },
+  init: function(appId, userId, options) {
+    this.appId = appId;
+    this.userId = userId;
+    $.extend(this.options, options);
+  },
 
-    notifications: function(callback) {
-      $.get(this.options.baseUrl + '/notifications?user_id=' + this.userId, function(data) {
+  notifications: function(callback) {
+    $.get(this.options.baseUrl + '/notifications?user_id=' + this.userId, function(data) {
+      callback(data);
+    });
+  },
+
+  acknowledgeNotification: function(notificationId, callback) {
+    $.ajax({
+      url: this.options.baseUrl + '/notifications/' + notificationId,
+      type: 'DELETE',
+      data: { user_id: this.options.userId }
+      success: function(data) {
         callback(data);
-      });
-    }
+      }
+    });
   }
+}
 
