78475c04f3053e24a582f446d0d541d757d0f157
kxhitiz <nirvanagrg@gmail.com>
2016-06-10 11:02:07 -0500

    Pass user sign in count on notifications api call.

--- src/echo.js
+++ src/echo.js
@@ -2,17 +2,19 @@ var echo = {
   options: {
     appId: 'dummyAppID',
     userId: 'dummyUserID',
+    userSignInCount: undefined,
     baseUrl: 'http://localhost:4000/api/v1'
   },
 
-  init: function(appId, userId, options) {
-    $.extend(this.options, options);
+  init: function(appId, userId, userSignInCount) {
+    //$.extend(this.options, options);
     this.options.appId = appId;
     this.options.userId = userId;
+    this.options.userSignInCount = userSignInCount;
   },
 
   notifications: function(callback) {
-    $.get(this.options.baseUrl + '/notifications?user_id=' + this.options.userId, function(data) {
+    $.get(this.options.baseUrl + '/notifications?user_id=' + this.options.userId + '&sign_in_count=' + this.options.userSignInCount, function(data) {
       callback(data);
     });
   },
